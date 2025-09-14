a6556da1d3aa09638bce3b62f9ff64163286ea9d
Jesus Rodriguez-Luna <112538272+JRodriguez-Luna@users.noreply.github.com>
2025-09-14 00:37:18 -0700

    Add optimized font loading with next/font

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+// You'll use this file to keep the fonts that will be used
+// throughtout the application.
+
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ 
+    weight: ['400', '700'],
+    subsets: ['latin'] 
+});
